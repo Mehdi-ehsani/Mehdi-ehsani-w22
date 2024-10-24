6719e0917866d3693661287b617f454c9e9c0055
Mehdi-ehsani <metti.work@gmail.com>
2024-10-24 23:23:00 +0330

    add login page style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../assets/image/logo.png"
+import styles from "./LoginPage.module.css"
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+   <div className={styles.container}>
+     <div className={styles.form}>
+      <img className={styles.logo} src={logo} alt='logo'/>
+      <h1>فرم ورود</h1>
+      <input type='text'placeholder='نام کاربری'/>
+      <input type='password' placeholder='رمز عبور'/>
+      <button className={styles.submitBtn} type='submit'>ورود</button>
+      <Link to="/registration">ایجاد حساب کاربری!</Link>
+    </div>
+   </div>
   )
 }
 
