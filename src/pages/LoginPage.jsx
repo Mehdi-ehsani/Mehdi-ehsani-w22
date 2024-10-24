d4b1e8af60d8ec6fb44dd6bde9a9e0eac0ce02fb
Mehdi-ehsani <metti.work@gmail.com>
2024-10-24 23:58:11 +0330

    add registration form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../assets/image/logo.png"
-import styles from "./LoginPage.module.css"
+import styles from "./Forms.module.css"
 
 const LoginPage = () => {
   return (
    <div className={styles.container}>
-     <div className={styles.form}>
+     <form className={styles.form}>
       <img className={styles.logo} src={logo} alt='logo'/>
       <h1>فرم ورود</h1>
       <input type='text'placeholder='نام کاربری'/>
       <input type='password' placeholder='رمز عبور'/>
       <button className={styles.submitBtn} type='submit'>ورود</button>
       <Link to="/registration">ایجاد حساب کاربری!</Link>
-    </div>
+    </form>
    </div>
   )
 }
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import styles from "./Forms.module.css"
+import logo from "../assets/image/logo.png"
+import { Link } from "react-router-dom"
 
 const RegistrationPage = () => {
   return (
-    <div>RegistrationPage</div>
+    <div className={styles.container}>
+     <form className={styles.form}>
+      <img className={styles.logo} src={logo} alt='logo'/>
+      <h1>فرم ثبت نام</h1>
+      <input type='text'placeholder='نام کاربری'/>
+      <input type='password' placeholder='رمز عبور'/>
+      <input type='password' placeholder=' تکرار رمز عبور'/>
+      <button className={styles.submitBtn} type='submit'>ثبت نام</button>
+      <Link to="/login">حساب کاربری دارید؟</Link>
+    </form>
+   </div>
   )
 }
 
