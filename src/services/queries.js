1ecd6f8b4edf3fa2f1f12e19a80c8b25be34ece5
Mehdi-ehsani <metti.work@gmail.com>
2024-10-26 21:55:15 +0330

    useProducts query config

---
+++ src/services/queries.js
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../configs/api";
+
+const useProducts = () => {
+	const queryFn = (data) => {
+	 return	api.get("products", data);
+	};
+	return useQuery({queryKey: ["products"] , queryFn});
+};
+export {useProducts}
